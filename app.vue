b6152c4cd8cfb9d12a71562c2b5ca6fd216468f6
Sinver Aguilo <sinveraguilo@gmail.com>
2024-01-20 22:39:10 -0400

    Add form validation and reset functionality

--- app.vue
+++ app.vue
@@ -1,6 +1,83 @@
 <script setup lang="ts">
 import IconDollar from '@/assets/icons/icon-dollar.svg?data'
 import IconPerson from '@/assets/icons/icon-person.svg?data'
+
+const errorMessages = {
+  bill: 'Can\'t be zero',
+  people: 'Can\'t be zero',
+}
+
+let bill = ref<number | null>(null)
+let tipRate = ref<number | null>(null)
+let customTipRate = ref<number | null>(null)
+let people = ref<number | null>(null)
+let tipAmountPerPerson = ref<number | null>(null)
+let totalTip = ref<number | null>(null)
+
+function setTipRateAndClearCustom(value: number) {
+  customTipRate.value = null
+  tipRate.value = value
+}
+
+function inputCustomTipRate(value: string) {
+  tipRate.value = Number(value)
+}
+
+watch([bill, tipRate, people], () => {
+  if (validateFields()) {
+    totalTip.value = calculateTipTotal()
+    tipAmountPerPerson.value = calculateTipAmountPerPerson()
+  }
+})
+
+let billErrorMessage = ref<string>('')
+let peopleErrorMessage = ref<string>('')
+function validateFields() {
+  billErrorMessage.value = ''
+  peopleErrorMessage.value = ''
+
+  if (bill.value === 0 || bill.value === null) {
+    billErrorMessage.value = errorMessages.bill
+  }
+  if (people.value === 0 || people.value === null) {
+    peopleErrorMessage.value = errorMessages.people
+  }
+
+  return !billErrorMessage.value && !peopleErrorMessage.value
+}
+
+function calculateTipAmountPerPerson() {
+  return totalTip.value! / people.value!
+}
+
+function calculateTipTotal() {
+  const tipAmount = (bill.value! * tipRate.value!) / 100
+  return tipAmount
+}
+
+
+const totalFormatted = computed(() => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(totalTip.value!)
+})
+
+const tipAmountPerPersonFormatted = computed(() => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(tipAmountPerPerson.value!)
+})
+
+function resetForm () {
+  bill.value = null
+  tipRate.value = null
+  customTipRate.value = null
+  people.value = null
+  tipAmountPerPerson.value = null
+  totalTip.value = null
+}
 </script>
 
 <template>
@@ -22,9 +99,11 @@ import IconPerson from '@/assets/icons/icon-person.svg?data'
           label="Bill"
           id="bill"
           placeholder="0"
-          errorMessage="Can't be zero"
+          :errorMessage="billErrorMessage"
           :icon="IconDollar"
           type="number"
+          v-model="bill"
+
         />
 
         <div>
@@ -33,12 +112,12 @@ import IconPerson from '@/assets/icons/icon-person.svg?data'
             id="tip"
             class="mt-4 grid grid-cols-2 desktop:grid-cols-3 gap-4 desktop:gap-x-3"
             role="group" aria-label="Select a tip or type a custom percentage">
-            <CommonsButton variant="secondary">5%</CommonsButton>
-            <CommonsButton variant="secondary">10%</CommonsButton>
-            <CommonsButton variant="secondary">15%</CommonsButton>
-            <CommonsButton variant="secondary">25%</CommonsButton>
-            <CommonsButton variant="secondary">50%</CommonsButton>
-            <CommonsTextField type="number" placeholder="Custom" />
+            <CommonsButton :variant="tipRate === 5 ? 'primary' : 'secondary'" @click="setTipRateAndClearCustom(5)">5%</CommonsButton>
+            <CommonsButton :variant="tipRate === 10 ? 'primary' : 'secondary'" @click="setTipRateAndClearCustom(10)">10%</CommonsButton>
+            <CommonsButton :variant="tipRate === 15 ? 'primary' : 'secondary'" @click="setTipRateAndClearCustom(15)">15%</CommonsButton>
+            <CommonsButton :variant="tipRate === 25 ? 'primary' : 'secondary'" @click="setTipRateAndClearCustom(25)">25%</CommonsButton>
+            <CommonsButton :variant="tipRate === 50 ? 'primary' : 'secondary'" @click="setTipRateAndClearCustom(50)">50%</CommonsButton>
+            <CommonsTextField type="number" placeholder="Custom" v-model="customTipRate" @input="inputCustomTipRate($event.target.value)" />
           </div>
         </div>
 
@@ -46,9 +125,10 @@ import IconPerson from '@/assets/icons/icon-person.svg?data'
           label="Number of People"
           id="people"
           placeholder="0"
-          errorMessage="Can't be zero"
+          :errorMessage="peopleErrorMessage"
           :icon="IconPerson"
           type="number"
+          v-model="people"
         />
 
 
@@ -63,7 +143,7 @@ import IconPerson from '@/assets/icons/icon-person.svg?data'
             <br />
             <span class="text-xs text-primary-grayish">/ person</span>
           </span>
-          <span class="text-3xl font-bold text-primary">$0.00</span>
+          <span class="text-3xl font-bold text-primary">{{ tipAmountPerPersonFormatted }}</span>
         </p>
         <p class="flex justify-between">
           <span class="text-base">
@@ -71,11 +151,11 @@ import IconPerson from '@/assets/icons/icon-person.svg?data'
             <br />
             <span class="text-xs text-primary-grayish">/ person</span>
           </span>
-          <span class="text-3xl font-bold text-primary">$0.00</span>
+          <span class="text-3xl font-bold text-primary">{{ totalFormatted }}</span>
         </p>
         </div>
 
-        <CommonsButton variant="primary" class="w-full mt-8">Reset</CommonsButton>
+        <CommonsButton variant="primary" class="w-full mt-8" @click="resetForm">Reset</CommonsButton>
 
       </div>
     </form>
